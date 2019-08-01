f85df6d96e827b1acb1a600884328f74f82f2c3d
Joao Eriberto Mota Filho <eriberto@eriberto.pro.br>
2019-08-01 17:16:00 -0300

    Changed version to 2.53

--- lib/HTML/TextToHTML.pm
+++ lib/HTML/TextToHTML.pm
@@ -1,6 +1,6 @@
 package HTML::TextToHTML;
 {
-  $HTML::TextToHTML::VERSION = '2.5201';
+  $HTML::TextToHTML::VERSION = '2.53';
 }
 use 5.8.1;
 use strict;
@@ -12,7 +12,7 @@ HTML::TextToHTML - convert plain text file to HTML.
 
 =head1 VERSION
 
-version 2.5201
+version 2.53
 
 =head1 SYNOPSIS
 
